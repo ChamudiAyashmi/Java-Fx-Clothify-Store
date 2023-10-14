4296f48aacd8fdbf06580f455b83d6c83cd27e21
ChamudiAyashmi <chamudi2003@gmail.com>
2023-10-14 22:34:50 +0530

    add new forms

--- src/main/java/org/example/controller/DashboardController.java
+++ src/main/java/org/example/controller/DashboardController.java
@@ -1,20 +1,30 @@
 package org.example.controller;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 public class DashboardController {
+    public Label lblOderCount;
+    public Label lblItemCount;
+    public Label lblSuppliersCount;
+    public Label lblEmployersCount;
     private Stage stage;
     public void btnOrdersOnAction(ActionEvent actionEvent) throws IOException {
         stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/order_form.fxml"))));
         stage.show();
     }
-    public void btnItemsOnAction(ActionEvent actionEvent) {
+    public void btnItemsOnAction(ActionEvent actionEvent) throws IOException {
+        stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/item_form.fxml"))));
+        stage.show();
 
     }
 
@@ -22,7 +32,10 @@ public class DashboardController {
 
     }
 
-    public void btnSuppliersOnAction(ActionEvent actionEvent) {
+    public void btnSuppliersOnAction(ActionEvent actionEvent) throws IOException {
+        stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/supplier_form.fxml"))));
+        stage.show();
 
     }
 
@@ -43,4 +56,5 @@ public class DashboardController {
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/login_form.fxml"))));
         stage.show();
     }
+
 }
--- src/main/java/org/example/controller/OrderFormController.java
+++ src/main/java/org/example/controller/OrderFormController.java
@@ -1,4 +1,9 @@
 package org.example.controller;
 
+import javafx.event.ActionEvent;
+
 public class OrderFormController {
+    public void btnPlaceOrderOnAction(ActionEvent actionEvent) {
+
+    }
 }
